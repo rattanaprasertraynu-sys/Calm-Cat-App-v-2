0df421fe015d3ba1ff459dae40ba24f5134a373f
rattanaprasertraynu-sys <rattanaprasertraynu@gmail.com>
2026-04-27 22:55:02 +0700

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { detectEmotion } from "@/lib/emotion";
-import { getReplySmart } from "@/lib/getReply";
+import { detectEmotion } from "../../../lib/emotion";
+import { getReplySmart } from "../../../lib/getReply";
 
 const emotionMap: any = {
   STRESSED: "sit_tail",
