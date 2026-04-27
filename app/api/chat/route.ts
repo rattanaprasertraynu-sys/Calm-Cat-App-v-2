a115cba3aef200f22f8ee71934e4431b0e490b03
rattanaprasertraynu-sys <rattanaprasertraynu@gmail.com>
2026-04-27 22:08:29 +0700

    Create route.ts

---
+++ app/api/chat/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { detectEmotion } from "@/lib/emotion";
+import { getReplySmart } from "@/lib/getReply";
+
+const emotionMap: any = {
+  STRESSED: "sit_tail",
+  SAD: "heart",
+  LONELY: "window",
+  NEUTRAL: "idle",
+  POSITIVE: "smile",
+};
+
+export async function POST(req: Request) {
+  const { message } = await req.json();
+
+  const emotion = detectEmotion(message);
+  const reply = getReplySmart(emotion, message);
+
+  return NextResponse.json({
+    reply,
+    emotion,
+    animation: emotionMap[emotion],
+  });
+}
