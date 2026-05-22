c7fcdbfaeea9e1ae55540a8e1fe3cdf0a6d9cd65
rattanaprasertraynu-sys <rattanaprasertraynu@gmail.com>
2026-05-22 16:40:43 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -830,29 +830,7 @@ runTimeout(() => {
       🌙 นุดยังอยากเล่าต่อไหม?
     </p>
 
-    <p
-  style={{
-    opacity: 0.7,
-    marginTop: 12,
-    marginBottom: 12,
-    fontSize: 14,
-  }}
->
-  🌙 หรือถ้านุดมีอะไร
-  อยากบอกใครบางคน...
-  ก็พิมพ์ไว้ตรงนี้ได้นะ
-</p>
-    
-    <textarea
-  placeholder="🌙 อยากบอกอะไรกับใครบางคนไหม..."
-  value={deepInput}
-  onChange={(e) =>
-    setDeepInput(
-      e.target.value
-    )
-  }
-  style={textareaStyle}
-/>
+  
     
     <div
       style={{
@@ -864,7 +842,7 @@ runTimeout(() => {
     >
       <button
        onClick={() => {
-
+  setDeepInput("");
    setDeepTalkText(
     randomItem(
       t.deepQuestions1
