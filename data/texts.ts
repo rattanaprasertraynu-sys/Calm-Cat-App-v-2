2c46720e0146aa035b471423dceae7946b7776d8
rattanaprasertraynu-sys <rattanaprasertraynu@gmail.com>
2026-06-01 15:30:57 +0700

    Update texts.ts

--- data/texts.ts
+++ data/texts.ts
@@ -1,1 +1,17 @@
+export const texts = {
+  splash: "Calm Cat",
 
+  greetingTitle: "สวัสดีนุด 🐱",
+
+  greetingMessage:
+    "วันนี้เจ้าส้มอยู่เป็นเพื่อนนะ",
+
+  diaryQuestion:
+    "ตอนนี้รู้สึกอย่างไรบ้าง",
+
+  farewell:
+    "คืนนี้ขอให้นุดหลับฝันดีนะ 🧡",
+
+  continue:
+    "แล้วเจอกันใหม่นะ",
+};
